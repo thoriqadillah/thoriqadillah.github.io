2331f58f995828cff9309549da1c54bed5caaf62
thoriqadillah <thoriqadillah59@gmail.com>
2023-01-19 23:49:17 +0700

    add navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'dark': "#010002",
+			'bright': '#f8efd4',
+			'main': "#d7385e",
+			'second': '#edc988'
+		},
+	},
+	plugins: [],
+}
