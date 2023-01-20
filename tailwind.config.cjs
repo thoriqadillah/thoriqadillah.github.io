45abaea2a38ceb8e6e829efde0d7abe343529122
thoriqadillah <thoriqadillah59@gmail.com>
2023-01-20 08:21:17 +0700

    refactor header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			'dark': "#010002",
-			'bright': '#f8efd4',
-			'main': "#d7385e",
-			'second': '#edc988'
+		extend: {
+			colors: {
+				'dark': "#010002",
+				'bright': '#f8efd4',
+				'main': "#d7385e",
+				'second': '#edc988'
+			},
 		},
 	},
 	plugins: [],
