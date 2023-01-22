217f09f5d0dbf2ff9be9a44b0d8c3bac3ff39ddc
thoriqadillah <thoriqadillah59@gmail.com>
2023-01-22 17:32:09 +0700

    project page and some fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			'black': "#010002",
-			'dark': '#242424',
-			'darker': "#121212",
-			'grey': "#373737",
-			'bright': '#f8efd4',
-			'white': '#FFFFFF',
-			'main': "#d7385e",
-			'second': '#edc988'
+		extend: {
+			colors: {
+				'black': "#010002",
+				'dark': '#242424',
+				'darker': "#121212",
+				'grey': "#373737",
+				'bright': '#f8efd4',
+				'white': '#FFFFFF',
+				'main': "#d7385e",
+				'second': '#edc988'
+			},
 		},
 	},
 	plugins: [],
