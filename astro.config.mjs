de6cb0243a9a1a57bbc56638c8103d1a323a54e5
thoriqadillah <thoriqadillah59@gmail.com>
2023-01-23 17:36:15 +0700

    change to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(), 
+    mdx()
+  ]
 });
