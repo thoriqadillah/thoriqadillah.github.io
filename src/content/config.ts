d17ae16bd0d1a59871bfb9efb4654e42c96d9ed4
thoriqadillah <thoriqadillah59@gmail.com>
2023-02-10 14:13:47 +0700

    add demo label

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const projects = defineCollection({
   schema: z.object({
     title: z.string(),
     stakeholder: z.string(),
+    containsDemo: z.boolean().optional(),
     articleDate: z.string(),
     projectStart: z.date(),
     projectFinish: z.date(),
